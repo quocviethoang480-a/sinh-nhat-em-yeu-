f8cbe37346c2f3946acdc3ead2e957883912dd1d
quocviethoang480-a <quocviethoang480@gmail.com>
2025-09-18 03:47:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -86,3 +86,17 @@ document.addEventListener("click", function (e) {
     heart.remove();
   }, 4000);
 });
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 2500); // đổi ảnh sau 2.5 giây
+}
